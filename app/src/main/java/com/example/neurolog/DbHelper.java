ef53c5621cbc2eaca8eb6424b484398eb1a83d8c
promoody1997-pixel <promoody1997@gmail.com>
2025-12-24 19:28:20 +0200

    Create DbHelper.java

---
+++ app/src/main/java/com/example/neurolog/DbHelper.java
@@ -0,0 +1,61 @@
+package com.example.neurolog;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "NeuroLog.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_LOGS = "logs";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+    public static final String COLUMN_REACTION_TIME = "reaction_time";
+    public static final String COLUMN_MOOD = "mood";
+    public static final String COLUMN_ENERGY = "energy";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_LOGS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TIMESTAMP + " TEXT,"
+                + COLUMN_REACTION_TIME + " INTEGER,"
+                + COLUMN_MOOD + " INTEGER,"
+                + COLUMN_ENERGY + " INTEGER" + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOGS);
+        onCreate(db);
+    }
+
+    public void insertLog(int reactionTime, int mood, int energy) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        
+        String timestamp = java.text.DateFormat.getDateTimeInstance().format(new java.util.Date());
+        
+        values.put(COLUMN_TIMESTAMP, timestamp);
+        values.put(COLUMN_REACTION_TIME, reactionTime);
+        values.put(COLUMN_MOOD, mood);
+        values.put(COLUMN_ENERGY, energy);
+
+        db.insert(TABLE_LOGS, null, values);
+        db.close();
+    }
+
+    public Cursor getAllLogs() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_LOGS, null, null, null, null, null, COLUMN_ID + " DESC");
+    }
+}
